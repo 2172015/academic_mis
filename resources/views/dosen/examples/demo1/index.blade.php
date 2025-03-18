8d28e2fc4e9abfd392da309932a4d98c0002a92c
ADV4-16 <2172015@maranatha.ac.id>
2025-03-18 13:09:56 +0700

    success index

--- resources/views/dosen/examples/demo1/index.blade.php
+++ resources/views/dosen/examples/demo1/index.blade.php
@@ -19,12 +19,12 @@
 	</script>
 
 	<!-- CSS Files -->
-	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../assets/css/plugins.min.css">
-	<link rel="stylesheet" href="../assets/css/kaiadmin.min.css">
+	<link rel="stylesheet" href={{ asset('/dosen/assets/css/bootstrap.min.css') }}>
+	<link rel="stylesheet" href={{ asset('/dosen/assets/css/plugins.min.css') }}>
+	<link rel="stylesheet" href={{ asset('/dosen/assets/css/kaiadmin.min.css') }}>
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="../assets/css/demo.css">
+	<link rel="stylesheet" href={{ asset('/dosen/assets/css/demo.css')}}>
 </head>
 <body>
 	<div class="wrapper">
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dosen.examples.demo1.index');
 });
 
 Route::get('/dashboard', function () {
